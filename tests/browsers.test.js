491cd995f97f595f0cefed78fc9ddc1f04202e33
Tanishka-Jagtap <tanishka-jagtap22@vit.edu>
2025-07-21 15:05:25 +0530

    feat: add test cases

--- tests/browsers.test.js
+++ tests/browsers.test.js
@@ -1,6 +1,18 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('Test browsers', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5173/');
+  await page.goto('http://localhost:5173/');
   await page.pause();
+});
+
+test('correct title', async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await expect(page).toHaveTitle(/Vite ReactJS — Starter/);
+});
+
+test('has heading', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByRole("heading", {
+    name:"Vite ReactTS — Starter",
+  })).toBeVisible();
 });
